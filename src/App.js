327beff35c1eacfca125ac87378886871e756504
Jin Hyeok Song <songwlsgur@gmail.com>
2021-03-29 23:23:46 +0900

    final

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          아침에 일어나면 파이팅하고 일조심히 가자
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          love~~
         </a>
       </header>
     </div>
